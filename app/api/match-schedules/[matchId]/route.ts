7b7cf4ef53bf681a9bdb511c5d5465b109025864
KagameYakito <gabriel.arelius@gmail.com>
2026-09-14 07:27:54 +0000

    ayo bisa

--- app/api/match-schedules/[matchId]/route.ts
+++ app/api/match-schedules/[matchId]/route.ts
@@ -126,7 +126,6 @@ export async function GET(
       status: schedule.status,
       schedulesSummaryFix: schedule.schedules_summary_fix,
       schedulesCustom: schedule.schedules_custom,
-      
       schedulesCustomRequest: schedule.schedules_custom_request || null,
       ulasan: schedule.ulasan || [],
       videoCall: schedule.video_call,
