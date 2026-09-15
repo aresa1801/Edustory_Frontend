946bfd9d1ca11c8de5413790b0e8475ac2d38550
aresa1801 <aresa1801@gmail.com>
2026-09-15 06:06:42 +0000

    ayo dong

--- components/reschedule-wizard.tsx
+++ components/reschedule-wizard.tsx
@@ -52,9 +52,9 @@ const TARGET_TIME_SLOTS = [
 ]
 
 function formatDateKey(d: Date): string {
-  const y = d.getUTCFullYear()
-  const m = String(d.getUTCMonth() + 1).padStart(2, '0')
-  const day = String(d.getUTCDate()).padStart(2, '0')
+  const y = d.getFullYear()
+  const m = String(d.getMonth() + 1).padStart(2, '0')
+  const day = String(d.getDate()).padStart(2, '0')
   return `${y}-${m}-${day}`
 }
 
